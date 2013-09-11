26e5bacaae429992a421f2a7c30840ee5e5af996
Brett Sheffield <brett@gladserv.com>
2013-09-11 17:40:34 +0100

    update view salesorder_current to fix #82

--- sql/gladbooks.business.sql
+++ sql/gladbooks.business.sql
@@ -537,27 +537,44 @@ AND (tr.valid_from <= salesorder_nextissuedate(so.id)
 AND (tr.valid_to >= salesorder_nextissuedate(so.id) OR tr.valid_to IS NULL)
 GROUP BY so.id, so.organisation, so.ordernum;
 
-
 CREATE OR REPLACE VIEW salesorder_current AS
-SELECT 
-	s.*,
-	s0.organisation,
-	s0.ordernum,
-	roundhalfeven(SUM(COALESCE(soi2.price, p2.price_sell) * soi2.qty),2) AS price,
+SELECT
+	so.id,
+	so.organisation,
+	so.ordernum,
+	sod.salesorder,
+	sod.quotenumber,
+	sod.ponumber,
+	sod.description,
+	sod.cycle,
+	sod.start_date,
+	sod.end_date,
+	sod.is_open,
+	sod.is_deleted,
+	roundhalfeven(SUM(COALESCE(soi.price, p.price_sell) * soi.qty),2) AS price,
 	roundhalfeven(COALESCE(tx.tax, '0.00'),2) as tax,
-	roundhalfeven(SUM(COALESCE(soi2.price, p2.price_sell) * soi2.qty) + 
-		COALESCE(tx.tax, '0.00'),2) AS total
-FROM salesorderdetail s
-LEFT JOIN salesorder_tax tx ON s.salesorder = tx.salesorder
-INNER JOIN salesorder s0 ON s0.id = s.salesorder
-INNER JOIN salesorderitem_current soi2 ON s.id = soi2.salesorder
-INNER JOIN product_current p2 ON p2.product = soi2.product
-WHERE s.id IN (
+	roundhalfeven(SUM(COALESCE(soi.price, p.price_sell) * soi.qty) +
+	                COALESCE(tx.tax, '0.00'),2) AS total
+FROM salesorder so
+INNER JOIN salesorderdetail sod ON so.id = sod.salesorder
+LEFT JOIN salesorderitem_current soi ON so.id = soi.salesorder
+INNER JOIN product_current p ON p.product = soi.product
+LEFT JOIN salesorder_tax tx ON sod.salesorder = tx.salesorder
+WHERE sod.id IN (
 	SELECT MAX(id)
 	FROM salesorderdetail
 	GROUP BY salesorder
 )
-GROUP BY s.id, s0.organisation, s0.ordernum, tx.tax
+GROUP BY so.id, so.organisation, so.ordernum, tx.tax,
+	sod.salesorder,
+	sod.quotenumber,
+	sod.ponumber,
+	sod.description,
+	sod.cycle,
+	sod.start_date,
+	sod.end_date,
+	sod.is_open,
+	sod.is_deleted
 ;
 
 /*  not used - intended for overriding taxes
