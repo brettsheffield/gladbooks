ec6c6858345fc75fe7f0acc7365b8f042a1e4d64
Brett Sheffield <brett@gladserv.com>
2013-10-17 11:13:46 +0100

    csvtoxml plugin

---
+++ src/csvtoxml/main.c
@@ -0,0 +1,139 @@
+/*
+ * csvtoxml - plugin for Gladbooks
+ *
+ * this file is part of GLADBOOKS
+ *
+ * Copyright (c) 2012, 2013 Brett Sheffield <brett@gladbooks.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program (see the file COPYING in the distribution).
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "main.h"
+#include <fcntl.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <libxml/parser.h>
+#include <libxml/xmlschemas.h>
+
+void check_args(int argc, char **argv)
+{
+        if (argc != 2) {
+                usage(EXIT_FAILURE);
+        }
+}
+
+int flattenxml(xmlDocPtr doc, char **xml, int pretty)
+{
+        xmlChar *xmlbuff;
+        int buffersize;
+
+        xmlDocDumpFormatMemoryEnc(doc, &xmlbuff, &buffersize, "UTF-8", pretty);
+        *xml = malloc(snprintf(NULL, 0, "%s", (char *) xmlbuff) + 1);
+        sprintf(*xml, "%s", (char *) xmlbuff);
+
+        xmlFree(xmlbuff);
+
+        return 0;
+}
+
+int main(int argc, char **argv)
+{
+        int fd;
+        ssize_t size = 1;
+        char c[1];
+        char f[LINE_MAX] = ""; /* field */
+        int lines = 0;
+        int quot = 0;
+        int len = 0;
+        int flds = 1;
+        xmlNodePtr n = NULL;
+        xmlNodePtr nrow = NULL;
+        xmlNodePtr nfld = NULL;
+        xmlNodePtr nval = NULL;
+        xmlDocPtr doc;
+        char *xml;
+
+
+        check_args(argc, argv);
+
+        /* open file for reading */
+        fd = open(argv[1], O_RDONLY);
+        if (fd == -1) {
+                perror("open()");
+                _exit(EXIT_FAILURE);
+        }
+
+        /* start building xml document */
+        doc = xmlNewDoc(BAD_CAST "1.0");
+        n = xmlNewNode(NULL, BAD_CAST "resources");
+        xmlDocSetRootElement(doc, n);
+
+        /* read file one character at a time */
+        while (size == 1) {
+                size = read(fd, c, 1);
+                if ((size == 1) && (flds == 1) && (len == 1))
+                        nrow = xmlNewNode(NULL, BAD_CAST "row");
+                if (c[0] == '"') {
+                        /* start/end quoted field */
+                        quot = (len == 0) ? 1 : 0;
+                }
+                else if ((((quot == 0) && (c[0] == ',')) 
+                || (c[0] == '\n') || (size == 0)) && (len > 0)) {
+                        /* unquoted comma or newline - end field, start new */
+        
+                        nfld = xmlNewNode(NULL, BAD_CAST "field");
+                        nval = xmlNewText(BAD_CAST f);
+                        xmlAddChild(nfld, nval);
+                        xmlAddChild(nrow, nfld);
+
+                        if (c[0] == '\n') {
+                                lines++;
+                                flds = 0;
+                                xmlAddChild(n, nrow);
+                        }
+                        flds++;
+                        len = 0;
+                }
+                else {
+                        f[len] = c[0];
+                        len++;
+                        if (len > LINE_MAX) {
+                                fprintf(stderr, "Line length overrun\n");
+                                _exit(EXIT_FAILURE);
+                        }
+                        f[len] = '\0';
+                }
+        }
+        close(fd);
+
+        flattenxml(doc, &xml, 1);
+        printf("%s", xml);
+
+        xmlFreeDoc(doc);
+        xmlCleanupParser();
+        free(xml);
+
+        return 0;
+}
+
+void usage(int status)
+{
+        fprintf(stderr, "usage: csvtoxml <filename>\n");
+        _exit(status);
+}
