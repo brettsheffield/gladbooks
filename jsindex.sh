e1fb0eacb2fbedacaf9281909e820728a63ab6df
Brett Sheffield <brett@gladserv.com>
2013-10-26 10:41:25 +0100

    updated jsindex.sh

--- jsindex.sh
+++ jsindex.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # print js function names and line numbers
-egrep -n '^function' js/gladbooks.js |sort -k2|cut -f1 -d\(|awk '{ print $2 " "  $1 }'|cut -f1 -d\:|less
+{ egrep -n '^function' js/gladbooks.js |sort -k2|cut -f1 -d\(|awk '{ print $2 " "  "gladbooks.js " $1 }'|cut -f1 -d\: ; 
+egrep -n '^function' js/gladd.js |sort -k2|cut -f1 -d\(|awk '{ print $2 " "  "gladd.js " $1 }'|cut -f1 -d\: ; } | sort | less
