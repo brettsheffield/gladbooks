859427bcdf6c60571af0b31d187b82d5477d045e
Brett Sheffield <brett@gladserv.com>
2014-01-23 16:49:26 +0000

    set PO edit tab title

--- js/gladbooks.js
+++ js/gladbooks.js
@@ -1747,7 +1747,7 @@ function submitJournalEntry(event, form, bankid) {
 /* override the gladd.js function which sets tab titles */
 tabTitle = function (title, object, action, xml) {
     var namedobjects = [ 'account', 'contact', 'department', 'division',
-        'organisation', 'product', 'salesorder'
+        'organisation', 'product', 'purchaseorder', 'salesorder'
     ];
     
     if (namedobjects.indexOf(object) != -1 && action == 'update' && xml[0]) {
@@ -1758,6 +1758,9 @@ tabTitle = function (title, object, action, xml) {
         }
         else if (object == 'product') {
             title = $(xml[0]).find('shortname').first().text();
+        }
+        else if (object == 'purchaseorder') {
+            title = 'PO/' + $(xml[0]).find('order').first().text();
         }
         else if (object == 'salesorder') {
             title = 'SO/' + $(xml[0]).find('order').first().text();
