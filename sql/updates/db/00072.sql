041b0dba999abfd5510df18b890aa0ca06d7f7da
Brett Sheffield <brett@gladserv.com>
2015-01-05 06:56:31 +0000

    use new VAT nominal codes for VAT return

--- sql/updates/db/00072.sql
+++ sql/updates/db/00072.sql
@@ -2,24 +2,25 @@ CREATE OR REPLACE FUNCTION prepare_vat_return(start_date DATE, end_date DATE)
 RETURNS INT4 AS
 $$
 DECLARE
-        q1     TEXT;
-        q2     TEXT;
-        q3     TEXT;
-        q4     TEXT;
-        q5     TEXT;
-        q6     TEXT;
-        q7     TEXT;
-        q8     TEXT;
-        q9     TEXT;
-        a1     NUMERIC;
-        a2     NUMERIC;
-        a3     NUMERIC;
-        a4     NUMERIC;
-        a5     NUMERIC;
-        a6     NUMERIC;
-        a7     NUMERIC;
-        a8     NUMERIC;
-        a9     NUMERIC;
+        q1      TEXT;
+        q2      TEXT;
+        q3      TEXT;
+        q4      TEXT;
+        q5      TEXT;
+        q6      TEXT;
+        q7      TEXT;
+        q8      TEXT;
+        q9      TEXT;
+        a1      NUMERIC;
+        a2      NUMERIC;
+        a3      NUMERIC;
+        a4      NUMERIC;
+        a5      NUMERIC;
+        a6      NUMERIC;
+        a7      NUMERIC;
+        a8      NUMERIC;
+        a9      NUMERIC;
+        cash    BOOLEAN;
 BEGIN
         DROP TABLE IF EXISTS vatreport;
         CREATE TEMP TABLE vatreport(
@@ -27,6 +28,9 @@ BEGIN
                 q TEXT,
                 a NUMERIC
         );
+        
+        -- Find out if we we are reporting VAT on a cash basis
+        SELECT vatcashbasis INTO cash FROM business WHERE id=current_business();
 
         q1 := 'VAT due in this period on sales and other outputs';
         q2 := 'VAT due in this period on acquisitions from other EC Member States';
@@ -37,28 +41,30 @@ BEGIN
         q7 := 'Total value of purchases and all other inputs excluding any VAT.  Include your box 9 figure.';
         q8 := 'Total value of all supplies of goods and related costs, excluding and VAT, to other EC Member States';
         q9 := 'Total value of acquisitions of goods and related costs excluding any VAT, from other EC Member States';
+      
+        SELECT COALESCE(SUM(l.credit),'0.00') - COALESCE(SUM(l.debit),'0.00') INTO a1 
+        FROM journal j
+        INNER JOIN ledger l ON j.id = l.journal
+        WHERE j.transactdate BETWEEN start_date AND end_date
+        AND l.account = '2200';
         
-        -- TODO: calculate on cash basis if required
-        SELECT SUM(tax) INTO a1
-        FROM salesinvoice_current
-        WHERE taxpoint BETWEEN start_date AND end_date;
-
         a2 := '0.00'; -- TODO
 
         a3 := a1 + a2;
 
-        -- TODO: calculate on cash basis if required
-        SELECT SUM(tax) INTO a4
-        FROM purchaseinvoice_current
-        WHERE taxpoint BETWEEN start_date AND end_date;
-
+        SELECT COALESCE(SUM(l.debit),'0.00') - COALESCE(SUM(l.credit),'0.00') INTO a4 
+        FROM journal j
+        INNER JOIN ledger l ON j.id = l.journal
+        WHERE j.transactdate BETWEEN start_date AND end_date
+        AND l.account = '2201';
+        
         a5 := a3 - a4;
 
-        SELECT SUM(subtotal) INTO a6
+        SELECT COALESCE(SUM(subtotal),'0.00') INTO a6
         FROM salesinvoice_current
         WHERE taxpoint BETWEEN start_date AND end_date;
 
-        SELECT SUM(subtotal) INTO a7
+        SELECT COALESCE(SUM(subtotal),'0.00') INTO a7
         FROM purchaseinvoice_current
         WHERE taxpoint BETWEEN start_date AND end_date;
 
