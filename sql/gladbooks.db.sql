34cc51464720533728db1b624553e20ae4741229
Brett Sheffield <brett@gladserv.com>
2015-03-21 12:02:21 +0000

    prevent allocating more than invoice total

--- sql/gladbooks.db.sql
+++ sql/gladbooks.db.sql
@@ -1071,48 +1071,61 @@ $check_transaction_balance$ LANGUAGE plpgsql;
 
 -- ensure payment allocations do not exceed amount of payment
 CREATE OR REPLACE FUNCTION check_payment_allocation()
-RETURNS trigger AS $check_payment_allocation$
+RETURNS trigger AS $$
 DECLARE
-	payment		NUMERIC;
-	allocated	NUMERIC;
-	type		TEXT;
-	paymentid	INT4;
-	idtable		TEXT;
-	detailtable	TEXT;
+        payment         NUMERIC;
+        allocated       NUMERIC;
+        invoicetotal    NUMERIC;
+        type            TEXT;
+        paymentid       INT4;
+        idtable         TEXT;
+        detailtable     TEXT;
 BEGIN
-	type := quote_ident(TG_ARGV[0]);
-	paymentid := NEW.payment;
+        type := quote_ident(TG_ARGV[0]);
+        paymentid := NEW.payment;
 
         -- check arguments --
-	IF type <> 'sales' AND type <> 'purchase' THEN
-		RAISE EXCEPTION '%I() called with invalid type', TG_NAME;
-	END IF;
+        IF type <> 'sales' AND type <> 'purchase' THEN
+                RAISE EXCEPTION '%I() called with invalid type', TG_NAME;
+        END IF;
 
-	-- find amount of payment --
-	idtable := type || 'payment';
-	detailtable := idtable || 'detail';
-	EXECUTE format('SELECT amount FROM %I WHERE id IN ', detailtable) ||
-	format('(SELECT MAX(id) FROM %I GROUP BY %I) ',detailtable,idtable) ||
-	format('AND %I=''%s'';', idtable, paymentid)
-	INTO payment;
+        -- find amount of payment --
+        idtable := type || 'payment';
+        detailtable := idtable || 'detail';
+        EXECUTE format('SELECT amount FROM %I WHERE id IN ', detailtable) ||
+        format('(SELECT MAX(id) FROM %I GROUP BY %I) ',detailtable,idtable) ||
+        format('AND %I=''%s'';', idtable, paymentid)
+        INTO payment;
 
-	-- how much have we allocated? --
-	idtable = type || 'paymentallocation';
-	detailtable := idtable || 'detail';
-	EXECUTE 'SELECT SUM(amount) ' ||
-	format('FROM %I WHERE id IN ', detailtable) ||
-	format('(SELECT MAX(id) FROM %I GROUP BY %I) ',detailtable,idtable) ||
-	format('AND payment=''%s'';', paymentid)
-	INTO allocated;
+        -- how much have we allocated? --
+        idtable = type || 'paymentallocation';
+        detailtable := idtable || 'detail';
+        EXECUTE 'SELECT SUM(amount) ' ||
+        format('FROM %I WHERE id IN ', detailtable) ||
+        format('(SELECT MAX(id) FROM %I GROUP BY %I) ',detailtable,idtable) ||
+        format('AND payment=''%s'';', paymentid)
+        INTO allocated;
 
-	IF allocated > payment THEN
-		RAISE EXCEPTION 'payment over-allocated';
-	END IF;
+        IF allocated > payment THEN
+                RAISE EXCEPTION 'payment over-allocated';
+        END IF;
 
-	RETURN NEW;
+        -- check we're not allocating more than the amount of the invoice
+        detailtable := type || 'invoice_current';
+        EXECUTE 'SELECT total ' ||
+        format('FROM %I ', detailtable) ||
+        format('WHERE id = ''%s''', NEW.salesinvoice)
+        INTO invoicetotal;
+
+        IF NEW.amount > invoicetotal THEN
+                RAISE EXCEPTION 'Cannot allocate more than total of invoice';
+        END IF;
+
+        RETURN NEW;
 
 END;
-$check_payment_allocation$ LANGUAGE plpgsql;
+$$ LANGUAGE plpgsql;
+
 
 -- trigger to ensure each period is only issued once per salesorder
 CREATE OR REPLACE FUNCTION check_salesorder_period()
